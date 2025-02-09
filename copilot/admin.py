be860d1b852b3f80a180ec0b3cde1b52ade3b088
amit-99 <amitbhjbl99@gmail.com>
2025-02-08 20:59:44 -0500

    Users model made userid UUID autogenerate

--- copilot/admin.py
+++ copilot/admin.py
@@ -3,6 +3,9 @@
 from django.contrib import admin
 from copilot.models import User, Expense, ExpenseSummary, Chat
 
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ('userId',)
+
 class ExpenseSummaryAdmin(admin.ModelAdmin):
     readonly_fields = ('formatted_data',)  # Make the data read-only
 
@@ -15,7 +18,7 @@ class ExpenseSummaryAdmin(admin.ModelAdmin):
     # Exclude the raw 'data' field from form display if it's causing issues
     exclude = ('data',)
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Expense)
 admin.site.register(ExpenseSummary, ExpenseSummaryAdmin)
 admin.site.register(Chat)
